ae80f2adfb16a422003fb54587ccd9f7d7550f63
Yahya <yahyakamran099@gmail.com>
2023-10-12 00:10:51 +0500

    Connected to database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connectDB = require("./Config/db");
+const dotenv = require("dotenv");
+
+dotenv.config();
+connectDB();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
